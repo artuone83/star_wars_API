bf73b83406ff716912ed94816392498637340927
Artur Wozniak <wozniaka83@gmail.com>
2018-10-23 17:16:33 +0200

    added Release Date

--- src/components/show_results.js
+++ src/components/show_results.js
@@ -5,7 +5,7 @@ export const axiosHtml = (txt) => `<li class="list-group-item">${txt}</li>`;
 export const showResultsAxios = (selectOption, result) => {
   let html;
   if(selectOption === 'films') {
-    html = result.map(result => axiosHtml(`<b>Title:</b> ${result.title} <b>director:</b> ${result.director}`) );
+    html = result.map(result => axiosHtml(`<b>Title:</b> ${result.title} <b>Director:</b> ${result.director} <b>Release Date:</b> ${result.release_date}`) );
 
   }else if (selectOption === 'people') {
     html = result.map(result => axiosHtml(`<b>Name:</b> ${result.name} <b>height:</b> ${result.height}`) );
